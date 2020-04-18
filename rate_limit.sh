06a8a715ef3463101aa9a5a5eb065a45c785d8c9
Tomas Bellus <tomas.bellus@gmail.com>
2020-04-18 20:58:06 +0200

    current iptables rate limiting

--- rate_limit.sh
+++ rate_limit.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# OBJECTIVE: rate limit the SSDP inboud traffic to evade SSDP DDoS amplification attack
+# add this to your initialization iptables script
 
-# ensure a chain exists and its empty
-# iptables -N SSDP_LOG_DROP
-iptables -F SSDP_LOG_DROP
-iptables -A SSDP_LOG_DROP -m limit --limit 5/minute -j LOG --log-ip-options --log-tcp-options --log-uid --log-prefix "SSDP-REQUEST-DROP " --log-level info
-iptables -A SSDP_LOG_DROP -m limit --limit 10/hour -j LOG --log-ip-options --log-tcp-options --log-uid --log-prefix "SSDP-REQUEST-DROP " --log-level info
-iptables -A SSDP_LOG_DROP -j DROP
+#iptables -F
+#iptables -X
 
-# iptables -I INPUT -m hashlimit -p udp --dport 1900 --hashlimit-above 3/second --hashlimit-mode srcip --hashlimit-name ssdp_per_second -j SSDP_LOG_DROP
+iptables -N SSDP_RATE_LIMIT
+	iptables -A SSDP_RATE_LIMIT -m hashlimit --hashlimit-above 5/minute --hashlimit-mode srcip --hashlimit-name ssdp_rate_limit -j DROP
+	iptables -A SSDP_RATE_LIMIT -m hashlimit --hashlimit-above 10/hour --hashlimit-mode srcip --hashlimit-name ssdp_rate_limit -j DROP
+	iptables -A SSDP_RATE_LIMIT -j ACCEPT
+# optionally jump to your custom LOG_DROP or LOG_ACCEPT chains
+
+# SSDP honeypot
+iptables -I INPUT -p udp --dport 1900 -j SSDP_RATE_LIMIT
+
+# UPNP honeypot
+iptables -I INPUT -p tcp --dport 5000 -j ACCEPT
+iptables -I INPUT -p tcp --dport 1784 -j ACCEPT
