9087870e7d9ac0226555fc412f122e61bb80752c
Tomas Bellus <tomas.bellus@gmail.com>
2020-04-30 17:41:53 +0200

    parametrize the rate limiting and apply logic to requests

--- honeypot/hon_ssdp.py
+++ honeypot/hon_ssdp.py
@@ -29,54 +29,66 @@ _SSDP_LISTEN_BIND_STR = f'{_SSDP_LISTEN_ADDR}:{_SSDP_LISTEN_PORT}'
 _SSDP_SOURCE_ADDR = config.get('source_addr')
 # SSDP 'HTTP OK' response headers
 _SSDP_HEADER_LOCATION = config['location_header']
+_SSDP_SECOND_RATELIMIT = config.getint('ratelimit_second', fallback=1)
+_SSDP_MINUTE_RATELIMIT = config.getint('ratelimit_minute', fallback=5)
+_SSDP_HOUR_RATELIMIT = config.getint('ratelimit_hour', fallback=10)
+_RATELIMIT_METRICS = {
+    'last_hour': _SSDP_HOUR_RATELIMIT,
+    'last_minute': _SSDP_MINUTE_RATELIMIT,
+    'last_second': _SSDP_SECOND_RATELIMIT
+}
 
 log = logging.getLogger('hon_ssdp')
 
-last_cleanup_time_mutex = Lock()
-ratelimit_stats_mutex = Lock()
-RATELIMIT_METRICS = {'last_hour': 10, 'last_minute': 5, 'last_second': 1}
-ratelimit_stats = {}
-last_cleanup_time = {x: datetime.now() for x in RATELIMIT_METRICS.keys()}
+__last_cleanup_time_mutex = Lock()
+__ratelimit_stats_mutex = Lock()
+__ratelimit_stats = {}
+__last_cleanup_time = {x: datetime.now() for x in _RATELIMIT_METRICS.keys()}
 
 
 def _increment_ratelimit_counters(ip):
-    with ratelimit_stats_mutex:
-        if ip not in ratelimit_stats:
-            ratelimit_stats[ip] = {m: 1 for m in RATELIMIT_METRICS.keys()}
+    with __ratelimit_stats_mutex:
+        if ip not in __ratelimit_stats:
+            __ratelimit_stats[ip] = {m: 1 for m in _RATELIMIT_METRICS.keys()}
         else:
-            for m in RATELIMIT_METRICS.keys():
-                ratelimit_stats[ip][m] += 1
+            for m in _RATELIMIT_METRICS.keys():
+                __ratelimit_stats[ip][m] += 1
 
 
 def _ratelimit_cleanup():
     from time import sleep
 
     def _ratelimit_cleanup_for_key(key: str):
-        log.debug(f'RATELIMIT | runnnig clean up for {key}')
-        with ratelimit_stats_mutex:
-            for ip in ratelimit_stats.keys():
-                ratelimit_stats[ip][key] = 0
+        with __ratelimit_stats_mutex:
+            to_delete = set()
+            for ip in __ratelimit_stats.keys():
+                __ratelimit_stats[ip][key] = 0
 
-        last_cleanup_time[key] = datetime.now()
+                ip_stats = __ratelimit_stats[ip]
+                if ip_stats['last_second'] == ip_stats['last_minute'] == ip_stats['last_hour'] == 0:
+                    to_delete.add(ip)
+
+            # delete the ip with all zero stats toremove historic data
+            for ip in to_delete:
+                del __ratelimit_stats[ip]
+
+        __last_cleanup_time[key] = datetime.now()
 
     while True:
         sleep(1)
-        with last_cleanup_time_mutex:
-            log.debug('RATELIMIT | running cleanup')
+        with __last_cleanup_time_mutex:
             _ratelimit_cleanup_for_key('last_second')
-            last_cleanup_time['last_second'] = datetime.now()
-            # fixme: change 10 to 60
-            if (datetime.now() - last_cleanup_time['last_minute']).total_seconds() >= 10:
+            __last_cleanup_time['last_second'] = datetime.now()
+            if (datetime.now() - __last_cleanup_time['last_minute']).total_seconds() >= 60:
                 _ratelimit_cleanup_for_key('last_minute')
-            # fixme: change 30 to 3600
-            if (datetime.now() - last_cleanup_time['last_hour']).total_seconds() >= 30:
+            if (datetime.now() - __last_cleanup_time['last_hour']).total_seconds() >= 3600:
                 _ratelimit_cleanup_for_key('last_hour')
 
 
 def ok_to_send(ip: str) -> bool:
-    with ratelimit_stats_mutex:
-        for m, lim in RATELIMIT_METRICS.items():
-            if ratelimit_stats[ip][m] > lim:
+    with __ratelimit_stats_mutex:
+        for m, lim in _RATELIMIT_METRICS.items():
+            if __ratelimit_stats[ip][m] > lim:
                 return False
 
     return True
@@ -84,7 +96,7 @@ def ok_to_send(ip: str) -> bool:
 
 # only for debugging
 def generate_ratelimiting_current_state_lines():
-    for ip, stats in ratelimit_stats.items():
+    for ip, stats in __ratelimit_stats.items():
         yield f'RATELIMIT | {ip};  sec: {stats["last_second"]};  min: {stats["last_minute"]};  hour: {stats["last_hour"]}'
 
 
@@ -113,10 +125,12 @@ class DummySSDPListener:
 
     async def __on_request(self, request_line: str, headers: MutableMapping[str, str]):
         _increment_ratelimit_counters(headers['_address'].split(':')[0])
-        raw_headers = '\r\n'.join([f'{k}:{v}' for k,v in headers.items() if not k.startswith('_')])
+        raw_headers = '\r\n'.join([f'{k}:{v}' for k, v in headers.items() if not k.startswith('_')])
         data = (request_line + raw_headers).encode()
         log.info(f'FROM {headers["_address"]} {len(data)} bytes - {data}')
-        if "M-SEARCH" in request_line and 'ST' in headers:
+        if "M-SEARCH" in request_line and \
+                'ST' in headers and \
+                ok_to_send(headers['_address'].split(':')[0]):
             await self.__on_msearch(headers)
 
     # Start listening for notifications
@@ -168,11 +182,12 @@ async def main_loop():
     await listener.async_start()
     try:
         while True:
-            # fixme: to this -> log.info(f'ALIVE idling for {_WATCHDOG_TIMEOUT / 60} minutes...')
-            # fixme: to this -> await asyncio.sleep(_WATCHDOG_TIMEOUT)
             for msg in generate_ratelimiting_current_state_lines():
                 log.debug(msg)
-            await asyncio.sleep(1)
+
+            log.debug(f'currently rate limiting {len(__ratelimit_stats.keys())} ips')
+            log.info(f'ALIVE idling for {_WATCHDOG_TIMEOUT / 60} minutes...')
+            await asyncio.sleep(_WATCHDOG_TIMEOUT)
     except KeyboardInterrupt:
         log.debug('KeyboardInterrupt')
         await listener.async_stop()
