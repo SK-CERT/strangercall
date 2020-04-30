dda9c4c7f5d4795e70ed8f6bc2996bf0857fbc3f
Tomas Bellus <tomas.bellus@gmail.com>
2020-04-30 15:16:46 +0200

    initial ratelimiting counter without checking before sending

--- honeypot/hon_ssdp.py
+++ honeypot/hon_ssdp.py
@@ -8,7 +8,9 @@ import configparser
 import logging
 import socket
 from async_upnp_client.ssdp import SsdpProtocol
+from datetime import datetime
 from sys import stdout
+from threading import Thread, Lock
 from typing import Any, Mapping, MutableMapping, Tuple, Callable, Awaitable
 
 from honeypot.services import generate_ssdp_httpok_packets
@@ -29,6 +31,61 @@ _SSDP_SOURCE_ADDR = config.get('source_addr')
 _SSDP_HEADER_LOCATION = config['location_header']
 
 log = logging.getLogger('hon_ssdp')
+
+last_cleanup_time_mutex = Lock()
+ratelimit_stats_mutex = Lock()
+RATELIMIT_METRICS = {'last_hour': 10, 'last_minute': 5, 'last_second': 1}
+ratelimit_stats = {}
+last_cleanup_time = {x: datetime.now() for x in RATELIMIT_METRICS.keys()}
+
+
+def _increment_ratelimit_counters(ip):
+    with ratelimit_stats_mutex:
+        if ip not in ratelimit_stats:
+            ratelimit_stats[ip] = {m: 1 for m in RATELIMIT_METRICS.keys()}
+        else:
+            for m in RATELIMIT_METRICS.keys():
+                ratelimit_stats[ip][m] += 1
+
+
+def _ratelimit_cleanup():
+    from time import sleep
+
+    def _ratelimit_cleanup_for_key(key: str):
+        log.debug(f'RATELIMIT | runnnig clean up for {key}')
+        with ratelimit_stats_mutex:
+            for ip in ratelimit_stats.keys():
+                ratelimit_stats[ip][key] = 0
+
+        last_cleanup_time[key] = datetime.now()
+
+    while True:
+        sleep(1)
+        with last_cleanup_time_mutex:
+            log.debug('RATELIMIT | running cleanup')
+            _ratelimit_cleanup_for_key('last_second')
+            last_cleanup_time['last_second'] = datetime.now()
+            # fixme: change 10 to 60
+            if (datetime.now() - last_cleanup_time['last_minute']).total_seconds() >= 10:
+                _ratelimit_cleanup_for_key('last_minute')
+            # fixme: change 30 to 3600
+            if (datetime.now() - last_cleanup_time['last_hour']).total_seconds() >= 30:
+                _ratelimit_cleanup_for_key('last_hour')
+
+
+def ok_to_send(ip: str) -> bool:
+    with ratelimit_stats_mutex:
+        for m, lim in RATELIMIT_METRICS.items():
+            if ratelimit_stats[ip][m] > lim:
+                return False
+
+    return True
+
+
+# only for debugging
+def generate_ratelimiting_current_state_lines():
+    for ip, stats in ratelimit_stats.items():
+        yield f'RATELIMIT | {ip};  sec: {stats["last_second"]};  min: {stats["last_minute"]};  hour: {stats["last_hour"]}'
 
 
 class DummySSDPListener:
@@ -55,6 +112,7 @@ class DummySSDPListener:
         return connect
 
     async def __on_request(self, request_line: str, headers: MutableMapping[str, str]):
+        _increment_ratelimit_counters(headers['_address'].split(':')[0])
         raw_headers = '\r\n'.join([f'{k}:{v}' for k,v in headers.items() if not k.startswith('_')])
         data = (request_line + raw_headers).encode()
         log.info(f'FROM {headers["_address"]} {len(data)} bytes - {data}')
@@ -110,8 +168,11 @@ async def main_loop():
     await listener.async_start()
     try:
         while True:
-            log.info(f'ALIVE idling for {_WATCHDOG_TIMEOUT / 60} minutes...')
-            await asyncio.sleep(_WATCHDOG_TIMEOUT)
+            # fixme: to this -> log.info(f'ALIVE idling for {_WATCHDOG_TIMEOUT / 60} minutes...')
+            # fixme: to this -> await asyncio.sleep(_WATCHDOG_TIMEOUT)
+            for msg in generate_ratelimiting_current_state_lines():
+                log.debug(msg)
+            await asyncio.sleep(1)
     except KeyboardInterrupt:
         log.debug('KeyboardInterrupt')
         await listener.async_stop()
@@ -125,6 +186,9 @@ def main():
     handler = logging.StreamHandler(stream=stdout)
     handler.setFormatter(formatter)
     log.addHandler(handler)
+
+    ratelimit_cleanup_thread = Thread(target=_ratelimit_cleanup, daemon=True)
+    ratelimit_cleanup_thread.start()
 
     loop = asyncio.get_event_loop()
     log.info(f'Started SSDP listener on {_SSDP_LISTEN_BIND_STR}')
